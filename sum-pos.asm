81c4bbbfb19401116caaf74202b187bc0d0130b4
Fawad205 <ahmadfawad5009@gmail.com>
2024-12-02 12:23:26 +0500

    PositiveSum

--- sum-pos.asm
+++ sum-pos.asm
@@ -1,18 +1,56 @@
-include irvine32.inc
-.data
+;First find positive number
+;then sum them 
+;and also telll how many elemenets positive 
+;1129
+;Ahmad FAwad
+include irvine32.inc  
+.data  
+array SWORD -3,-6,-1,-10,10,30,40,4  
+count Dword 0
+msgSum byte "Sum of positive number  ",0 
+msgCount byte "Total positive number in given array  ",0
 
-; array and other variables declaration here
+.code  
+main proc  
+call clrscr
 
-.code
-main proc
+mov eax,0
+mov esi,offset array
+mov ecx,lengthof array
 
+next:
+test word ptr [esi],8000h             ;Find positive number by test command 
 
-;code here
+pushfd
 
+add esi,type array                   ;mov next element in array
+popfd
 
+jz sum                                ;;jump if zero flag  on
 
-exit
-main endp
+recall:
+loop next 
 
+jmp print
 
-end main
+sum:
+inc count                            ; ;This count can count positibe numbers
+sub esi,type array                   ;go back element which check confition for
+add ax,[esi]   
+add esi,type array
+jmp recall
+
+print:
+mov edx,offset msgSum
+call writestring 
+call writeint
+call crlf
+mov edx,offset msgCount
+call writestring 
+mov eax,0
+mov eax,count
+call writedec
+
+exit  
+main endp  
+end main 
